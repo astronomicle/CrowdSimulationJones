27008bfa04934b1b57e340b80b7db9ac4aad968b
astronomicle <719168@student.dusd.net>
2024-03-19 13:51:05 -0700

    on flocking pt 2
    i did most of everything but now on flocking pt 2, 0 mins

--- CrowdSimulation/Assets/AIControl.cs
+++ CrowdSimulation/Assets/AIControl.cs
@@ -5,17 +5,60 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
 
-    void Start() {
+    void Start()
+    {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0, 10));
+        ResetAgent();
+    }
 
-        agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if(Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position).normalized;
+            Vector3 newGoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+
+            if(path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
     }
 
 
-    void Update() {
-
+    void Update()
+    {
+        if(agent.remainingDistance < 1)
+        {
+            ResetAgent();
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
     }
 }
